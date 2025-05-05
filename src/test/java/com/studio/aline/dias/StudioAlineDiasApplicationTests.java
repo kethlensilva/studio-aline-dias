346a6d55b5d113979179b8ea349e0d9cfebb40af
Kethlen Chrispim <kethlen_isa@hotmail.com>
2025-05-05 12:08:18 -0300

    alteração

---
+++ src/test/java/com/studio/aline/dias/StudioAlineDiasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studio.aline.dias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioAlineDiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
